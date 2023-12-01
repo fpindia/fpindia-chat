2a8f896fd8ebe49088ee61014c638d3997e64ee7
Sridhar Ratnakumar <srid@srid.ca>
2023-12-01 15:08:27 -0500

    Harden the server

--- hosts/fpindia-chat.nix
+++ hosts/fpindia-chat.nix
@@ -1,6 +1,18 @@
 { pkgs, ... }:
 
 {
+  # Harden.
+  services.openssh = {
+    settings = {
+      PasswordAuthentication = false;
+      PermitRootLogin = "no";
+    };
+    allowSFTP = false;
+  };
+  security.auditd.enable = true;
+  security.audit.enable = true;
+  networking.firewall.enable = true;
+
   environment.systemPackages = with pkgs; [
     htop
     ncdu
